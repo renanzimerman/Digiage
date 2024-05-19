af3969f12831de9ba98a384931a394b34b859bef
Renan Zimerman Leite <renanzimermanleite@gmail.com>
2024-05-19 17:53:27 -0300

    Update funcionario_idade.sql

--- TaskSQL/funcionario_idade.sql
+++ TaskSQL/funcionario_idade.sql
@@ -1,12 +1,11 @@
 SELECT 
+    YEAR(birth_date) AS ano,
     birth_date,
     gender,
     COUNT(DISTINCT emp_no) AS quantidade
 FROM 
     employees_2
 GROUP BY 
+    YEAR(birth_date),
     birth_date,
-    gender
-ORDER BY 
-    birth_date, 
     gender;
